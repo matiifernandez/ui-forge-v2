758209e2fc1b9a1a9660fcc60a292a526366a632
Lorenzo Panneman <unframed3@gmail.com>
2025-11-19 12:56:32 +0900

    did messages controller stuff

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+  def create
+    @component = Component.find(params[:component_id])
+    @message = Message.new(message_params)
+    @message.chat = @component.chat
+    @message.role = "user"
+    if @message.save
+      redirect_to
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
